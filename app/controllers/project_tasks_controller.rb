8c46ace50e975dbc58897d9b606fbe83768f9e27
Michael Newman <coloradohokie@gmail.com>
2020-03-26 10:23:03 -0600

    include tasks in project_tasks index endpoint

--- app/controllers/project_tasks_controller.rb
+++ app/controllers/project_tasks_controller.rb
@@ -1,8 +1,8 @@
 class ProjectTasksController < ApplicationController
 
     def index
-        @project_tasks - ProjectTask.all
-        render json: @project_tasks
+        @project_tasks = ProjectTask.all
+        render json: @project_tasks, include: :task
     end
 
 end
