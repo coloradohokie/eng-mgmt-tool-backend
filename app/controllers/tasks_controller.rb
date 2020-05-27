e5cbd9b226eb41519b12b995095fdb67bc9575b1
Michael Newman <coloradohokie@gmail.com>
2020-05-26 18:02:34 -0600

    add create and update methods to tasks controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,4 +5,30 @@ class TasksController < ApplicationController
         render json: @tasks
     end
 
+    def create
+        @task = Task.create(
+            project_id: params[:project_id],
+            name: params[:name],
+            template_name: params[:template_name],
+            done: false,
+            active: true,
+            sort_id: 99
+        )
+
+        render json: @task, status: :accepted
+    end
+
+    def update
+        @task = Task.find(params[:id])
+        @task.update(
+            project_id: params[:project_id],
+            name: params[:name],
+            template_name: params[:template_name],
+            done: params[:done],
+            sort_id: params[:sort_id],
+            active: params[:active]
+        )
+        render json: @task, json: Task.find(params[:id]), status: :accepted
+    end
+
 end
