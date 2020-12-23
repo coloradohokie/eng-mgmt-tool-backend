a3ada506ceffb17ca35cde093b43d258fb22c29e
Michael Newman <coloradohokie@gmail.com>
2020-12-23 16:16:50 -0700

    allow create users without authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+    skip_before_action :authenticate, only: [:create]
+    
     def create
         @user = User.create(
             username: params[:username],
