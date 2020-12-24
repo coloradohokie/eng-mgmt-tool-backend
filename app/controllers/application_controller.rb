8d39a853015b1dbb3ec625590bddbdd996895a90
Michael Newman <coloradohokie@gmail.com>
2020-12-24 13:38:34 -0700

    update authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::API
         header = request.headers["Authorization"]
 
         if !header
-            render json: {error: "no token present"}, status: :unauthorized
+            render json: {error: "no token present"}, status: :forbidden
         else
             begin
                 token = header.split(" ")[1]
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::API
                 payload = JWT.decode(token, secret)[0]
                 @user =  payload["user_id"]
             rescue
-                render json: {error: "Bad token", header: header, secret: Rails.application.secrets.secret_key_base, config: ENV['SECRET_KEY_BASE'] }, status: :unauthorized
+                render json: {error: "Bad token" }, status: :forbidden
             end
         end
     end
