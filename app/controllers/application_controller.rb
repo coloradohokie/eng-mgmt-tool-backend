79633e3f0c8c34f0d21fb97e556f677fc6c1b0b1
Michael Newman <coloradohokie@gmail.com>
2020-12-23 17:44:18 -0700

    remove authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::API
 
-    before_action :authenticate
+    # before_action :authenticate
 
     def authenticate
         authorization_header = request.headers["Authorization"]
