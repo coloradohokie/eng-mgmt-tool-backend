940682b84c7f46fed6a4212987fad125bab294e8
Michael Newman <coloradohokie@gmail.com>
2020-12-23 23:31:05 -0700

    update to find bug in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,20 +3,21 @@ class ApplicationController < ActionController::API
     before_action :authenticate
 
     def authenticate
-        authorization_header = request.headers["Authorization"]
+        header = request.headers["Authorization"]
 
-        if !authorization_header
-            render json: {error: "No token present"}, status: :unauthorized
+        if !header
+            render json: {error: "no token present"}, status: :unauthorized
         else
             begin
-                token = authorization_header.split(' ')[1]
+                token = header.split(" ")[1]
                 secret = Rails.application.secret_key_base
-                payload = JWT.decode(token, secret).first
+                payload = JWT.decode(token, secret)[0]
+                
                 @user = User.find payload["user_id"]
             rescue
-                render json: { error: "Invalid Token" }, status: :unauthorized
+                render json: {error: "Bad token"}, status: :unauthorized
             end
         end
     end
-
 end
+
