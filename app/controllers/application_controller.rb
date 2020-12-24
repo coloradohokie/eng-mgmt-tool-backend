d696b9c82e1bf2bf244b335972f25d073b5ca986
Michael Newman <coloradohokie@gmail.com>
2020-12-24 12:30:38 -0700

    add byebug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,9 +9,11 @@ class ApplicationController < ActionController::API
             render json: {error: "no token present"}, status: :unauthorized
         else
             begin
+                byebug
                 token = header.split(" ")[1]
                 secret = Rails.application.secrets.secret_key_base
                 payload = JWT.decode(token, secret)[0]
+                @user =  payload["user_id"]
             rescue
                 render json: {error: "Bad token"}, status: :unauthorized
             end
