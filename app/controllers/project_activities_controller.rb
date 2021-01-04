b2ff1e254845814375cb2fd061d83698b7ea59c7
Michael Newman <coloradohokie@gmail.com>
2021-01-04 11:21:14 -0700

    validate project activities

--- app/controllers/project_activities_controller.rb
+++ app/controllers/project_activities_controller.rb
@@ -6,19 +6,19 @@ class ProjectActivitiesController < ApplicationController
     end
 
     def create
-        @activity = ProjectActivity.create(
-            activity_id: params[:activity_id],
-            project_id: params[:project_id],
-            activity_date: params[:activity_date],
-            notes: params[:notes],
-            important: params[:important],
-            archived: params[:archived]
-        )
+        @activity = ProjectActivity.create(project_activities_params)
         render json:@activity, include: [:project, :activity], status: 200
     end
 
     private
     def project_activities_params
-        params.require(:project_activity).permit(:activity_id, :project_id, :activity_date, :notes, :important, :archived)
+        params.require(:project_activity).permit(
+            :activity_id, 
+            :project_id, 
+            :activity_date, 
+            :notes, 
+            :important, 
+            :archived
+        )
     end
 end
